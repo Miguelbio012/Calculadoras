0032872157564783ab6264507e427e11fe1508f7
Luis Miguel <138018833+Miguelbio012@users.noreply.github.com>
2024-08-16 19:47:29 -0300

    Update desvcalc.py

--- JuroCompostos/desvcalc.py
+++ JuroCompostos/desvcalc.py
@@ -1,9 +1,9 @@
 # Solicita o valor do capital
 
-capital = float(input("O valor do capital inserido, será de:"))
+capital = float(input("O valor do capital inserido, será de: "))
 
 # Solicita o valor da taxa de juros
-taxa = float(input("O valor da taxa será de:"))
+taxa = float(input("O valor da taxa será de: "))
 
 #Tipo de taxa
 taxa_tipo = input("A taxa que foi inserida está em mês ou em ano? (Digite 'm' para mensal e 'a' para anual): ").lower()
@@ -18,14 +18,14 @@ elif taxa_tipo == 'm':
     # Considera a taxa mensal
     taxaJC = taxa / 100
 else:
-    print("Entrada inválida para o tipo de taxa. Por favor, digite 'm' para mensal ou 'a' para anual.")
+    print("Entrada inválida para o tipo de taxa. Por favor, digite 'm' para mensal ou 'a' para anual: ")
     exit()
 
 # Taxa para exclusivamente Juros Compostos.
 # taxaJC = taxa / 100
 
 # Nessa primeira variável tempo, ele será indicado em ANOS.
-tempoAnos = int(input("O tempo em anos será de:"))
+tempoAnos = int(input("O tempo em anos será de: "))
 
 # Transformação de anos -> meses.
 tempoMes = tempoAnos * 12
