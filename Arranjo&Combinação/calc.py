caee8833310318b4e12b9fd53301ae7a17f542d5
Luis Miguel <138018833+Miguelbio012@users.noreply.github.com>
2024-08-17 12:24:44 -0300

    Update calc.py

--- Arranjo&Combinação/calc.py
+++ Arranjo&Combinação/calc.py
@@ -2,7 +2,7 @@
 import math #Essa biblioteca ja está imbutida no python e será usada para transformar os números em fatoriais
 
 # Escolha para saber qual método de resolução será usado
-tipoQuestao = input("A questão será solucionada com o método de ARRANJO ou Combinação? (Digite 'a' para arranjo e 'c' para combinação) ").lower()
+tipoQuestao = input("A questão será solucionada com o método de ARRANJO ou Combinação? (Digite 'a' para arranjo e 'c' para combinação e 'cs' para combinação simples) ").lower()
 
 #Condição para realização das fórmulas
 if tipoQuestao == 'a' :
@@ -33,6 +33,13 @@ elif tipoQuestao == 'c' :
 
     print(f"O resultado dessa questão usando o método de combinação foi de: {int(Combinacao)} combinações.")
 
-else :
-    print ("O resultado que foi inserido não é valido, por favor tente novamente.").exit()
+elif tipoQuestao == 'cs' : # Para combinação simples usando apenas fatorial
+    # Usará apenas a formula de fatorial comum
+    quest_fat = int(input("Qual numero deseja fazer a combinação simples ?? "))
+    # Agora irá transformar o número inserido em seu fatorial 
+    fat_Total = math.factorial(quest_fat)
 
+    print(f"O resultado do seu fatorial será de {int(fat_Total)}") 
+
+else :
+    print("O valor inserido não foi identificado, por favor tente novamente").exit()
